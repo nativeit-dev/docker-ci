e1fa21bcdcf177cf323331f92ac92bc2b0c972c6
Stephen Davis <sdavis@nativeit.net>
2022-06-17 03:03:15 -0400

    Added tools, actions

---
+++ tools/cosign/test/ci.mk
@@ -0,0 +1,19 @@
+############
+# signing ci
+############
+
+.PHONY: sign-ci-containers
+sign-ci-containers: ko
+	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/cosign:$(GIT_HASH)
+	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/sget:$(GIT_HASH)
+
+.PHONY: sign-ci-keyless-containers
+sign-ci-keyless-containers: ko
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosign:$(GIT_HASH)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/cosign:$(GIT_VERSION)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/sget:$(GIT_HASH)
+	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/sget:$(GIT_VERSION)
+
+.PHONY: sign-blob-experimental
+sign-blob-experimental:
+	./test/sign_blob_test.sh
---
+++ tools/crane/doc/crane_copy.md
@@ -0,0 +1,26 @@
+## crane copy
+
+Efficiently copy a remote image from src to dst while retaining the digest value
+
+```
+crane copy SRC DST [flags]
+```
+
+### Options
+
+```
+  -h, --help   help for copy
+```
+
+### Options inherited from parent commands
+
+```
+      --insecure            Allow image references to be fetched without TLS
+      --platform platform   Specifies the platform in the form os/arch[/variant][:osversion] (e.g. linux/amd64). (default all)
+  -v, --verbose             Enable debug logs
+```
+
+### SEE ALSO
+
+* [crane](crane.md)	 - Crane is a tool for managing container images
+
